616c0ea377436bb6f74b10df8d1f57b2f92493bf
Dan Appel <daniel.m.appel@gmail.com>
2017-09-05 12:17:20 -0700

    Conditoinal Harvester-Solution
    Working Solution of Conditional Harvester

--- APCS-Karel-Unit-1/lab_3/pair_programming/Problem1.java
+++ APCS-Karel-Unit-1/lab_3/pair_programming/Problem1.java
@@ -15,6 +15,24 @@ public class Problem1 extends Robot
     }
 
     public void checkSurrounded(){
+        //Initially facing North
+        if(frontIsClear()) {
+            turnLeft();
+            //Facing West
+            if(frontIsClear()) {
+                turnLeft();
+                //Facing south
+                if(frontIsClear()) {
+                  turnLeft();   
+                  //Facing East
+                  if(frontIsClear()) {
+                      turnLeft();
+                      
+                    }
+                    
+                }
+            }
+        }
         
     }
 }
